2c5479fafdaf1cbd6aa03e84a412abc451f3e0a6
diegomatah <diegoa.matah@gmail.com>
2022-07-09 17:10:15 +0000

    tarea simple counter lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bigcounter d-flex flex-direction-row justify-content-evenly" style={{"font-size":"90px","backgroundColor":"blue","display":"inline-block","color":"white","height":"120px","lineHeight":"120px"}}>
+		<div className="calendar"><i class="fas fa-clock"></i></div>
+		<div className="four">{props.digitfour % 10}</div>
+		<div className="three">{props.digitthree % 10}</div>
+		<div className="two">{props.digittwo % 10}</div>
+		<div className="one">{props.digitone % 10}</div>
 		</div>
 	);
 };
+
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,14 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval (function(){
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    counter++;
+    ReactDOM.render(<Home digitone={one} digittwo={two} digitthree={three} digitfour={four} />, 
+    document.querySelector("#app"));
+},1000);
+
